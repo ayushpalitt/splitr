785b6a5bb8055fc5a70f4a6e8935708a97d2c3c2
ayushpalitt <palitayush1234@gmail.com>
2025-08-08 11:25:25 +0530

    Update with work done over these days

---
+++ app/(main)/contacts/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react'
+
+const ContactsPage = () => {
+  return (
+    <div> ContactsPage
+      
+    </div>
+  )
+}
+
+export default ContactsPage
+ Con
--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,32 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin" ]})
 
 export const metadata = {
   title: "Splitr",
-  description: "Money Management within group.",
+  description: "The smartest way to split expenses with friends",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} `}
       >
-        {children}
+      <ClerkProvider>
+       <ConvexClientProvider>
+        <Header />
+
+       <main className="min-h-screen"> {children}</main>
+      </ConvexClientProvider>
+      </ClerkProvider>
       </body>
     </html>
   );
