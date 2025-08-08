8b03f7da88d8c8415fd197dca3c85598d5f85fd0
ayushpalitt <palitayush1234@gmail.com>
2025-08-08 15:45:08 +0530

    contacts landing page

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import { ConvexClientProvider } from "@/components/convex-client-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin" ]})
 
@@ -24,7 +25,10 @@ export default function RootLayout({ children }) {
        <ConvexClientProvider>
         <Header />
 
-       <main className="min-h-screen"> {children}</main>
+       <main className="min-h-screen"> 
+        {children}
+        <Toaster  richColors/>       
+        </main>
       </ConvexClientProvider>
       </ClerkProvider>
       </body>
--- convex/users.js
+++ convex/users.js
@@ -1,4 +1,4 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const store = mutation({
   args: {},
@@ -33,5 +33,24 @@ export const store = mutation({
       email: identity.email,
       imageURL: identity.pictureUrl,
     });
+  },
+});
+
+export const getCurrentUser = query({
+  handler:async() => {
+    const identity = await ctx.auth.getUserIdentity();
+    if(!identity) {
+      throw new Error("Not Authenticated")
+    }
+
+    const user = await ctx.db.query("users").withIndex("by_token", (q)=>{
+          q.eq("tokenIdentifier", identity.tokenIdentifier);
+
+    }).first();
+
+    if(!user) {
+      throw new Error("User Not Found");
+    }
+    return user;
   },
 });
